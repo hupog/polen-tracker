41070daf5f4c0fa1f781967fc0e276bb0702de79
Hugo González <hugogonzalez798@gmail.com>
2026-07-10 20:46:57 +0200

    fix formats

--- applications/pollen-collector/src/main/java/com/hugogonzalez/polentracker/collector/source/PollenSourceAdapter.java
+++ applications/pollen-collector/src/main/java/com/hugogonzalez/polentracker/collector/source/PollenSourceAdapter.java
@@ -1,2 +1,12 @@
-package com.hugogonzalez.polentracker.collector.source; import com.hugogonzalez.polentracker.domain.*; import com.hugogonzalez.polentracker.messaging.CollectionParameters; import java.util.List;
-public interface PollenSourceAdapter {PollenSourceType supportedSource(); List<PollenMeasurement> collect(CollectionParameters parameters);}
+package com.hugogonzalez.polentracker.collector.source;
+
+import com.hugogonzalez.polentracker.domain.*;
+import com.hugogonzalez.polentracker.messaging.CollectionParameters;
+
+import java.util.List;
+
+public interface PollenSourceAdapter {
+    PollenSourceType supportedSource();
+
+    List<PollenMeasurement> collect(CollectionParameters parameters);
+}
